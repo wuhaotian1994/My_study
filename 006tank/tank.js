0c4f6752392c7163942067b9e95ff49f1ee5ed01
sky <wuhaotian1994@outlook.com>
2019-08-22 13:17:17 +0800

    work

--- 006tank/tank.js
+++ 006tank/tank.js
@@ -1,8 +1,37 @@
-var cv = document.getElementById("box");
-var context=cv.getContext("2d");
-
+var cv;
+var context;
 var tk_me=new Image();
+var tk_me_x=0;
+var tk_me_y=0;
 tk_me.src="../img/tank.png"
-tk_me.onload=function(){
-	context.drawImage(tk_me,0,0);
+
+window.onload=function(){
+	cv = document.getElementById("box");
+	context=cv.getContext("2d");
+	context.drawImage(tk_me,tk_me_x,tk_me_y,20,15);
+	// var tk_timer=setInterval(function(){
+	// 	tk_me_y ++
+	// 	context.clearRect(0,0,600,400);
+	// 	context.drawImage(tk_me,tk_me_x,tk_me_y,20,15);
+	// },500);
+	document.onkeydown=function(event){
+		if(event.keyCode =="37")
+		{
+			tk_me_x--;
+		}
+		if(event.keyCode =="39")
+		{
+			tk_me_x++;
+		}
+		if(event.keyCode =="38")
+		{
+			tk_me_y--;
+		}
+		if(event.keyCode =="40")
+		{
+			tk_me_y++;
+		}
+		context.clearRect(0,0,600,400);
+		context.drawImage(tk_me,tk_me_x,tk_me_y,20,15);
+	}
 }
