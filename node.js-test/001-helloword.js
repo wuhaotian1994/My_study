1744d64396d4b7880bca1c94472940db2677a63d
sky <wuhaotian1994@outlook.com>
2019-09-02 21:19:44 +0800

    home

--- node.js-test/001-helloword.js
+++ node.js-test/001-helloword.js
@@ -1,7 +1,9 @@
 var http=require('http');
 
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end(../006tank/tank.html);
-}).listen(8611);
-console.log("Server runing... port is 8611");
+var server=http.createServer();
+server.on('request',function(){
+
+});
+server.listen('8611',function(){
+    console.log("Server runing... port is 8611");
+});
