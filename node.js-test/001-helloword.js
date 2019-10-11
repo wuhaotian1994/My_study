2cfb5328b730e8b6df261e26f8ca7dfc1de95e9c
sky <wuhaotian1994@outlook.com>
2019-10-12 00:24:22 +0800

    home

--- node.js-test/001-helloword.js
+++ node.js-test/001-helloword.js
@@ -1,9 +1,18 @@
 var http=require('http');
+var urlmodel=require('url');
 
 var server=http.createServer();
-server.on('request',function(){
+server.on('request',function(req,res){
+    const {pathname: url,query}=urlmodel.parse(req.url,true)
+    var _data={
+        name:'xgl',
+        sex:'女',
+        age:22
+    }
 
+    res.end(`${query.func}(${JSON.stringify(_data)})`)
 });
 server.listen('8611',function(){
     console.log("Server runing... port is 8611");
+    
 });
